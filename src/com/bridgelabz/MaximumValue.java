8921d821d79341912df5fef9feeb8996144bb9f8
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-21 12:33:35 +0530

    Added UC2 Find Maximum value between three Float number using Generics

--- src/com/bridgelabz/MaximumValue.java
+++ src/com/bridgelabz/MaximumValue.java
@@ -13,12 +13,26 @@ public class MaximumValue
 			return max;
 				
 		}
+		// this method is using comparing between three Integers...
+				public static Float findMaxFloat(Float number1, Float number2, Float number3)
+				{
+					Float max = number1;
+					if(number2.compareTo(max) >= 0)
+						max=number2;
+					if(number3.compareTo(max) >= 0)
+						max=number3;
+					return max;
+						
+				}
+		
+	
 
 		public static void main(String[] args) 
 		{
 			System.out.println("Find Maximum number using genric method ::");
 			// method calling findMaxInteger()
 			System.out.println("The maximum value between three Integer ::"+findMaxInteger(11,22,33));
+			System.out.println("The maximum value between three Float ::"+findMaxFloat(1.1f,2.2f,3.3f));
 		}
 
 	}
