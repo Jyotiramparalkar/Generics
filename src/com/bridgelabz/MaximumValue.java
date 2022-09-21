3f0ffaf803cbd7f7f8d00b52323bacc038c5fcf1
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-21 16:15:26 +0530

    Added UC4 Find maximum with more than three parameters using generics

--- src/com/bridgelabz/MaximumValue.java
+++ src/com/bridgelabz/MaximumValue.java
@@ -1,57 +1,45 @@
 package com.bridgelabz;
-// To find a maximum value using genrics
-public class MaximumValue 
+// Programm that using genrics method comparing between 4 parameters
+public class MaximumValue<T extends Comparable >
 {
-	// this method is using comparing between three Integers...
-	public static Integer findMaxInteger(Integer number1, Integer number2, Integer number3)
+	public T val1;
+	public T val2;
+	public T val3;
+	public T val4;
+	public MaximumValue (T val1, T val2, T val3, T val4)
 	{
-		Integer max = number1;
-		if(number2.compareTo(max) >= 0)
-			max=number2;
-		if(number3.compareTo(max) >= 0)
-			max=number3;
+		this.val1=val1;
+		this.val2=val2;
+		this.val3=val3;
+		this.val4=val4;
+	}
+	public T Maximum() {
+		return MaximumValue.findMaximum (val1, val2, val3, val4);	
+	}
+	//compare any type of data and find maximum
+	public static <T extends Comparable> T findMaximum(T val1, T val2, T val3, T val4)
+	{
+		T max = val1;
+		if(val2.compareTo(max)>=0)
+			max = val2;
+		if(val3.compareTo(max)>=0)
+			max = val3;
+		if(val4.compareTo(max)>=0)
+			max=val4;
 		return max;
-
 	}
-	// this method is using comparing between Float...
-	public static Float findMaxFloat(Float number1, Float number2, Float number3)
+	
+	public static void main (String [] args)
 	{
-		Float max = number1;
-		if(number2.compareTo(max) >= 0)
-			max=number2;
-		if(number3.compareTo(max) >= 0)
-			max=number3;
-		return max;
-
+		System.out.println("Find the maximum problem using Generics");
+		// Find Integer maximum value 
+		MaximumValue<Integer> maxIntger = new MaximumValue<>(10,20,30,40);
+		System.out.println("The maximum value between three Integer is  ::"+maxIntger.Maximum());
+		//Find Float maximum value
+		MaximumValue<Float> maxFloat = new MaximumValue<>(10.0f,20.0f,30.0f,40.0f);
+		System.out.println("The maximum value between float is  ::"+maxFloat.Maximum());
+		//Find String maximum value
+		MaximumValue<String> maxString = new MaximumValue<>("Grapes","Banana","peach","Lemon");
+		System.out.println("The maximum value of String is  ::"+maxString.Maximum());
 	}
-	// this method is using comparing between string
-	
-	public static String findMaxString(String val1, String val2, String val3){
-        String max = val1;
-        if (val2.compareTo(max) >= 0)
-            max = val2;
-        if (val3.compareTo(max) >= 0)
-            max = val3;
-
-        return  max;
-    }
-	
-
-
-
-	public static void main(String[] args) 
-	{
-		System.out.println("Find Maximum number using genric method ::");
-		// method calling findMaxInteger()
-		
-		System.out.println("The maximum value between three Integer :: "+findMaxInteger(11,22,33));
-		// method calling findMaxFloat()
-		
-		System.out.println("The maximum value between three Float :: "+findMaxFloat(1.1f,2.2f,3.3f));
-		// method calling findMaxString()
-		
-		System.out.println("The maximum value between the three string is :: " + findMaxString("Apple", "Peach", "Banana"));
-	}
-	
-
 }
