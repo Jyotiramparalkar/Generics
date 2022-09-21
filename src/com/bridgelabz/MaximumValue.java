b36cf66301dbd8ac7766570cbf0eca6ec594967d
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-21 23:22:02 +0530

    Refactor 2 find Maximum value using Genric class

--- src/com/bridgelabz/MaximumValue.java
+++ src/com/bridgelabz/MaximumValue.java
@@ -1,26 +1,46 @@
 package com.bridgelabz;
 // Programm that using genrics method comparing between 3 parameters
-public class MaximumValue
+public class MaximumValue<T extends Comparable> 
 {
-	 public static <T extends Comparable<T>> T findMaximum(T val1, T val2, T val3)
-	 {
-	        T max = val1;
-	        if (val2.compareTo(max) >= 0)
-	            max = val2;
-	        if (val3.compareTo(max) >= 0)
-	            max = val3;
 
-	        return  max;
-	    }
-	
-	public static void main (String [] args)
-	{
-		System.out.println("Find the maximum problem using Generics");
-		// Find Integer maximum value 
-		System.out.println("The maximum value between the three integer is :: " + findMaximum(10,20,30));
-		//Find Float maximum value
-		System.out.println("The maximum value between the three float is :: " + findMaximum(10.0f,20.0f,30.0f));
-		//Find String maximum value
-		System.out.println("The maximum value between the three String is : " + findMaximum("Apple", "Peach", "Banana"));
-	}
+    private T val1;
+    private T val2;
+    private T val3;
+    public MaximumValue(T val1, T val2, T val3)
+    {
+        this.val1 =val1;
+        this.val2 = val2;
+        this.val3 =val3;
+    }
+
+    public T maximux()
+    {
+        return MaximumValue.findMaximum(val1, val2, val3);
+    }
+
+    
+     //Generic method to compare any type of data and find maximum
+   
+    public static <T extends Comparable> T findMaximum(T val1, T val2, T val3)
+    {
+        T max = val1;
+        if (val2.compareTo(max) >= 0)
+            max = val2;
+        if (val3.compareTo(max) >= 0)
+            max = val3;
+
+        return  max;
+    }
+    public static void main(String[] args) {
+        System.out.println("Find Maximum Problem using Generics");
+
+        MaximumValue<Integer> maxInteger = new MaximumValue<>(10,20,30);
+        System.out.println("The maximum value between the three integer is : " + maxInteger.maximux());
+
+        MaximumValue<Float> maxFloat = new MaximumValue<>(10.0f, 20.0f, 30.0f);
+        System.out.println("The maximum value between the three float is : " + maxFloat.maximux());
+
+        MaximumValue<String> maxString = new MaximumValue<>("Apple", "Peach", "Banana");
+        System.out.println("The maximum value between the three string is : " + maxString.maximux());
+    }
 }
