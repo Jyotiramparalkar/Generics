4837a577b3cab03f5c195e3aaff2eaec72cda629
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-21 23:16:41 +0530

    Refactor 1 Finding Maximum value using 1 method

--- src/com/bridgelabz/MaximumValue.java
+++ src/com/bridgelabz/MaximumValue.java
@@ -1,51 +1,26 @@
 package com.bridgelabz;
-// Programm that using genrics method comparing between 4 parameters
-public class MaximumValue<T extends Comparable >
+// Programm that using genrics method comparing between 3 parameters
+public class MaximumValue
 {
-	public T val1;
-	public T val2;
-	public T val3;
-	public T val4;
-	public MaximumValue (T val1, T val2, T val3, T val4)
-	{
-		this.val1=val1;
-		this.val2=val2;
-		this.val3=val3;
-		this.val4=val4;
-	}
-	public T Maximum() {
-		return MaximumValue.findMaximum (val1, val2, val3, val4);	
-	}
-	//compare any type of data and find maximum
-	public static <T extends Comparable> T findMaximum(T val1, T val2, T val3, T val4)
-	{
-		T max = val1;
-		if(val2.compareTo(max)>=0)
-			max = val2;
-		if(val3.compareTo(max)>=0)
-			max = val3;
-		if(val4.compareTo(max)>=0)
-			max=val4;
-		printMax(max);
-		return max;
-	}
-	//Genric Method to print the maximum value
-		public static<T> void printMax(T val)
-		{
-			System.out.println("Maximum value is ::"+ val);
-		}
+	 public static <T extends Comparable<T>> T findMaximum(T val1, T val2, T val3)
+	 {
+	        T max = val1;
+	        if (val2.compareTo(max) >= 0)
+	            max = val2;
+	        if (val3.compareTo(max) >= 0)
+	            max = val3;
+
+	        return  max;
+	    }
 	
 	public static void main (String [] args)
 	{
 		System.out.println("Find the maximum problem using Generics");
 		// Find Integer maximum value 
-		MaximumValue<Integer> maxIntger = new MaximumValue<>(10,20,30,40);
-		System.out.println("The maximum value between three Integer is  ::"+maxIntger.Maximum());
+		System.out.println("The maximum value between the three integer is :: " + findMaximum(10,20,30));
 		//Find Float maximum value
-		MaximumValue<Float> maxFloat = new MaximumValue<>(10.0f,20.0f,30.0f,40.0f);
-		System.out.println("The maximum value between float is  ::"+maxFloat.Maximum());
+		System.out.println("The maximum value between the three float is :: " + findMaximum(10.0f,20.0f,30.0f));
 		//Find String maximum value
-		MaximumValue<String> maxString = new MaximumValue<>("Grapes","Banana","peach","Lemon");
-		System.out.println("The maximum value of String is  ::"+maxString.Maximum());
+		System.out.println("The maximum value between the three String is : " + findMaximum("Apple", "Peach", "Banana"));
 	}
 }
